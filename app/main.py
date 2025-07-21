11b671f872e947b10c0edc287096de3c54f2d0e1
EuwinC <euwincheung3@gmail.com>
2025-07-21 17:21:20 +0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,4 +1,17 @@
 import socket  # noqa: F401
+import threading
+
+def handle_ping(client_socket):
+    try:
+        while True:
+            request: bytes = client_socket.recv(512)
+            data: str = request.decode()
+
+            # print(data)
+            if "ping" in data.lower():
+                client_socket.send("+PONG\r\n".encode())
+    finally:
+        client_socket.close()
 
 
 def main():
@@ -6,18 +19,18 @@ def main():
     print("Logs from your program will appear here!")
 
     # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    client_socket, _ = server_socket.accept()  # wait for client
+    try:
+        while True:
+            client_socket, addr = server_socket.accept()  # Wait for a client
+            print(f"New connection from {addr}")
+            # Start a new thread for each client
+            client_thread = threading.Thread(target=handle_ping, args=(client_socket,))
+            client_thread.start()
+    finally:
+        server_socket.close()
 
-    while True:
-        request: bytes = client_socket.recv(512)
-        data: str = request.decode()
 
-        # print(data)
-        if "ping" in data.lower():
-            client_socket.send("+PONG\r\n".encode())
-
-
+    
 if __name__ == "__main__":
     main()
